a3a291c2e4778daba2d3d6ae00bdb9d8fbc13584
anthony-2903 <ajanampa@cudesi.com.pe>
2025-02-28 17:16:56 -0500

    act

--- components/Services.tsx
+++ components/Services.tsx
@@ -42,7 +42,7 @@ export default function Services() {
         },
 
         {
-          src: "/img/BOTFOTOTERAPIA.png",
+          src: "/img/BOi-FOTOTERAPIA.png",
           subtitle: "BIOFOTOTERAPIA",
           content:
             "Este tratamiento utiliza tecnología de luz LED para regenerar la piel, combatir manchas, mejorar cicatrices de acné y estimular la producción de colágeno. Es indoloro, no invasivo y apto para todo tipo de pieles.",
